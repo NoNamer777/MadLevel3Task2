80174b59443e45526c34089a47c70953fc3c6e0d
Oscar Wellner <o.wellner@telfort.nl>
2020-09-18 00:21:37 +0200

    Added layout Portal Overview Fragment and navigation to Add Portal Fragment

--- app/src/main/java/com/nonamer777/madlevel3task2/MainActivity.kt
+++ app/src/main/java/com/nonamer777/madlevel3task2/MainActivity.kt
@@ -2,13 +2,34 @@ package com.nonamer777.madlevel3task2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        // Listen to button clicks on the Floating Action Button.
+        fab.setOnClickListener {
+            navController.navigate(R.id.action_portalOverviewFragment_to_addPortalFragment)
+        }
+
+        toggleFab()
+    }
+
+    /** Hides the Floating Action Button when the user navigates towards the Add Portals Fragment. */
+    private fun toggleFab() = navController.addOnDestinationChangedListener { _, destination, _ ->
+
+        if (destination.id in arrayOf(R.id.addPortalFragment)) fab.hide()
+        else fab.show()
     }
 }
--- app/src/main/java/com/nonamer777/madlevel3task2/PortalOverviewFragment.kt
+++ app/src/main/java/com/nonamer777/madlevel3task2/PortalOverviewFragment.kt
@@ -5,14 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.nonamer777.madlevel3task2.model.Portal
+import com.nonamer777.madlevel3task2.model.PortalAdapter
+import kotlinx.android.synthetic.main.fragment_portal_overview.*
 
 /** A simple [Fragment] subclass that holds the overview of portals. */
 class PortalOverviewFragment : Fragment() {
+
+    private val portals = arrayListOf<Portal>()
+    private val portalAdapter = PortalAdapter(portals)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_portal_overview, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+    }
+
+    /** Initializes the view, connects the Portal Adapter to the RecyclerView. */
+    private fun initViews() {
+        rvPortals.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rvPortals.adapter = portalAdapter
     }
 }
