702d70b0f2965d733b1feefc72685bc87f0f305a
Oscar Wellner <o.wellner@telfort.nl>
2020-09-18 12:20:16 +0200

    Created layout and logic for adding a portal to the list of portals, and setup navigation back to the overview fragment

--- app/src/main/java/com/nonamer777/madlevel3task2/AddPortalFragment.kt
+++ app/src/main/java/com/nonamer777/madlevel3task2/AddPortalFragment.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_portal.*
+
+const val ARG_PORTAL_NAME = "portal_name"
+const val ARG_PORTAL_URL = "portal_url"
 
 /** A simple [Fragment] subclass to add portals to the overview of portals. */
 class AddPortalFragment : Fragment() {
@@ -14,5 +21,32 @@ class AddPortalFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_add_portal, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnAddPortal.setOnClickListener {
+            onAddPortal()
+        }
+    }
+
+    private fun onAddPortal() {
+        val portalName = inputPortalName.text.toString()
+        val portalUrl = inputPortalUrl.text.toString()
+        val args = Bundle()
+
+        if (portalName.isNotBlank() && portalUrl.isNotBlank()) {
+            args.putString(ARG_PORTAL_NAME, portalName)
+            args.putString(ARG_PORTAL_URL, portalUrl)
+
+            findNavController()
+                .navigate(R.id.action_addPortalFragment_to_portalOverviewFragment, args)
+        } else {
+            Toast.makeText(
+                activity,
+                R.string.not_valid_portal, Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
